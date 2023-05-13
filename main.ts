63aef298ce8cc204a6ab01f53d312c19b3acfb35
vanilla120707 <131329871+vanilla120707@users.noreply.github.com>
2023-05-13 09:37:40 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,5 @@
+input.onGesture(Gesture.ScreenDown, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
+})
+basic.showString("Hello!")
+basic.showIcon(IconNames.Happy)
